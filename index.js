f62aa201d6d3973f3fe0a9196c8424abb3f7351e
hamamullaiqi <hamamullaiqi@gmail.com>
2024-03-01 20:36:31 +0700

    fix:content

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import express from "express";
-
-const app = express();
-const PORT = 5500;
-
-app.get("/", (req, res) => {
-    res.send("Hello Express");
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("Hello Express");
